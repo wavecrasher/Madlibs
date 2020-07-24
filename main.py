4296b5ba5bea91a9cd27ebdcb3c462ba5e7a173a
Hannah Evans <wavecrasherhge@gmail.com>
2020-07-24 18:18:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+name = input("Hello, what is your name? \n")
+print(f"Hello {name}! Let's start.")
+#print("Hello" + name + "! Let's start")
+
+print()
+
+animals = input("Enter the name of a animal (plural): ")
+app = input("Enter a app: ")
+tv_show = input("Enter your favorite tv show? ")
+favoite_number = input("What is your favorite number? ")
+fast_food_chain = input("Enter a fast food chain: ")
+sport = input ("Enter a sport: ")
+favorite_game = input("Enter your favorite board game: ")
+name = input("Enter a name: ")
+time = input("Enter a time: ")
+
+print(f"Every day at noon the farmer will attend to his {favoite_number} {animals}. After attending to his {animals} he scrolls through his phone and opens {app}. Then he turns on the tv and re-enacts his favorite scene from {tv_show}! Then at {time}am the  farmer will settle down for some {fast_food_chain} with his daughter {name}. At {time}pm the farmer and {name} will play {sport} or {favorite_game}. Finally at 9pm both will head to bed ")
